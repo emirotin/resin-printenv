3496ed4389d482acde1aa70883a6c94395b6c3a6
Eugene Mirotin <emirotin@gmail.com>
2015-03-24 20:46:41 +0300

    logs

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var express = require('express'),
 app.use(compression());
 app.use(morgan('development'));
 app.use(function(req, res, next) {
+  console.log('Request received');
   res.json(env);
 });
 
 app.listent(8080);
+console.log('Listening on 8080');

00e79e8ee49e4741528f2927466eff482f899dfc
Eugene Mirotin <emirotin@gmail.com>
2015-03-24 18:52:16 +0300

    init

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+  compression = require('compression'),
+  app = express(),
+  env = process.env;
+
+app.use(compression());
+app.use(function(req, res, next) {
+  res.json(env);
+});
+
+app.listent(8080);

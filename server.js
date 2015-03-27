eee89df9f66f5ddf66cbbd47c7aab4208dc0c212
Eugene Mirotin <emirotin@gmail.com>
2015-03-28 01:29:43 +0300

    get env on each request

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require('express'),
   compression = require('compression'),
   morgan = require('morgan'),
-  app = express(),
-  env = process.env;
+  app = express();
 
 app.use(compression());
 app.use(morgan('dev'));
 app.use(function(req, res, next) {
-  res.json(env);
+  res.json(process.env);
 });
 
 app.listen(8080);
